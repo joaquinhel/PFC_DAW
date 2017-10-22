44df2dd3e1347af39d43854e79c7fb8c728e94c0
Joaquín <Joaqun@192.168.1.14>
2017-10-22 17:45:37 +0200

    Incluimos base de datos

--- WEB/inicio.php
+++ WEB/inicio.php
@@ -5,6 +5,7 @@
         <link href="../CSS/inicio.css" rel="stylesheet" type="text/css"/>
         <link href="../CSS/boton.css" rel="stylesheet" type="text/css"/>
         <title>Optica</title>
+        <script type="text/javascript" src="http://www.google.com/jsapi?key=AIzaSyB7tG99cRwOAj-7_Oifs_BFPtxTkrjBuXM"></script>
         <meta charset="UTF-8">
     </head>
     <body id="body">
@@ -16,13 +17,26 @@
 
             <div class="fila">
                 <div class="caja">
-                    CONSULTE A NUESTROS PROFESIONALES
-                    <a class="button" href="#">PEDIR CITA</a>
+
+                    <p><img class="iconos" src="../imagenes/iconos/informacion.jpg" alt="info"/><br />
+                        PONGASE EN CONTACTO CON NUESTROS PROFESIONALES <br /><br />
+                        <a class="button" href="#">PEDIR CITA</a>
+                    </p>
 
                 </div>
-                <div class="caja">Caja 11</div>
                 <div class="caja">
-                    <a class="button" href="#">OFERTAS</a>
+                    <p>
+                        <img id='logo' src="../imagenes/centroOptico.png" alt="logo"/><br />
+                        <img class="iconos" src="../imagenes/iconos/facebook.jpg" alt="face"/>
+                        <img class="iconos" src="../imagenes/iconos/whatsaap.png" alt="whatsaap"/>
+                        <img class="iconos" src="../imagenes/iconos/correo.jpg" alt="correo"/>
+                    </p>
+                </div>
+                <div class="caja">
+                    <p><img class="iconos" src="../imagenes/iconos/oferta.jpg" alt="oferta"/><br />
+                        CONSULTE NUESTROS ARTÍCULOS EN PROMOCIÓN <br /><br />
+                        <a class="button" href="#">OFERTAS</a>
+                    </p>
                 </div>
             </div>
 
@@ -31,19 +45,29 @@
             </div>
             <div class="fila">
                 <div class="caja">
-                    LOCALÍZANOS EN <br/>
-                    Avenida de Andalucía, 2  <br/>
-                    18600, Motril, Granada  <br/>
-                    Tel.: 958 60 59 20  <br/>
-                    Email: info@opticamilan.es
+                    <p>
+                        <img class="iconos" src="../imagenes/iconos/localizacion.png" alt="localizacion"/> <br/>
+                        LOCALÍZANOS EN <br/>
+                        Av. Principe de Asturias, 1 
+                        C.P. 30007, Murcia, Frente al Zig Zag
+                        Tel.: 968 967 011
+                        Movil. : 644 12 93 93
+                        Email: sanchezcentrooptico@gmail.com
+                    </p>
                 </div>
-                <div class="caja">Caja 8</div>
+                <div  class="caja">
+                    <iframe id='mapa'src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12577.415373564436!2d-1.1388460736019763!3d37.99220628628155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1cc50b66fdf84237!2sCentro+%C3%93ptico+S%C3%A1nchez!5e0!3m2!1ses!2ses!4v1508681401262"  ></iframe>
+                </div>
                 <div class="caja">
-                    NUESTRO HORARIO ES <br/>
-                    De lunes a viernes
-                    9:30-13:30 y 17:00-20:30  <br/>
-                    Sábados
-                    9:30-13:30
+
+                    <p>
+                        <img class="iconos" src="../imagenes/iconos/reloj.jpg" alt="reloj"/><br/>
+                        NUESTRO HORARIO ES <br />
+                        De lunes a viernes
+                        10:00 - 14:00 y 17:00 - 21:00  
+                        Sábados
+                        10:00 - 14:00
+                    </p>
                 </div>
             </div>
         </div>
